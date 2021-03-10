f3c09129de1d2ceb9cb44a0408d4f89026218709
Ohr42 <ohr79@mail.ru>
2021-03-11 00:15:00 +0700

    Новый Git

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -4,7 +4,6 @@ import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
